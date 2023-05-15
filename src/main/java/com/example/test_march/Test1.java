88be1c03d1962bbdef4f004ef088cc3723f96dcd
OleksandrShevchenko21 <shevchenko2106@gmail.com>
2023-05-15 09:45:52 +0300

    3

---
+++ src/main/java/com/example/test_march/Test1.java
@@ -0,0 +1,8 @@
+package com.example.test_march;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("test1");
+    }
+}
+
--- src/main/java/com/example/test_march/modelTest2/test2.java
+++
@@ -1,4 +0,0 @@
-package com.example.test_march.modelTest2;
-
-public class test2 {
-}
